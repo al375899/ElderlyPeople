d54ce5962519c24c957420db6bbdbc40c3cb8b43
al375899 <al375899@uji.es>
2020-06-03 16:06:23 +0200

    add contract and update

--- src/main/java/es/uji/ei1027/elderlypeople/dao/ContractDao.java
+++ src/main/java/es/uji/ei1027/elderlypeople/dao/ContractDao.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import es.uji.ei1027.elderlypeople.model.Contract;
-import es.uji.ei1027.elderlypeople.model.Request;
-
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
